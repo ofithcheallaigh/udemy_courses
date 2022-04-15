3f1164ede580bb822578548e4231ee9203afcd75
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-04-15 22:21:02 +0100

    adding comments about strings and slicers to the file

---
+++ The Python Bible/email_slicer.py
@@ -0,0 +1,6 @@
+# A slice is a small peice of something larger.
+# Remember: strings are interable.
+# Strings are made of elements, and each element has a number, starting at zero
+# These numbers are referred to an the index
+# Strings are immutable
+# 
