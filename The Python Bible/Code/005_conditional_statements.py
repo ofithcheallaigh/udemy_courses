13ebfa6133f60b1674d492baa8135304c5680ca5
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-04-16 19:32:27 +0100

    basic conditional examples - if, elif

---
+++ The Python Bible/Code/005_conditional_statements.py
@@ -0,0 +1,22 @@
+num1 = 100
+num2 = 100
+
+if num1 > num2:
+    print("num1 is greater than num2")
+elif num2 > num1:
+    print("num2 is greater than num1")
+else:
+    print("num1 and num2 are equal")
+
+# ---------------------------------------------
+num1 = 1
+num2 = 3
+
+if num1 > num2:
+    print("num1 is bigger than num2")
+
+elif num1 == num2:
+    print("num1 is equal to num2")
+
+elif num2 > num1:
+    print("num2 is bigger than num1")
