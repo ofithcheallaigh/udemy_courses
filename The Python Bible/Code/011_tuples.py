3ea3a2a72f9da7cb910d1fecb8e33c1e0be72c2a
sofithcheallaigh <sofithcheallaigh@gmail.com>
2022-04-20 19:41:22 +0100

    adding initial tupes file

---
+++ The Python Bible/Code/011_tuples.py
@@ -0,0 +1,6 @@
+# Tuples are data types which work similar to lists.
+# The only difference is when a tuple is created, it cannot be changed.
+# They are immuatable
+
+our_tuple = 1,2,3,'A','B','C'
+print(our_tuple)
