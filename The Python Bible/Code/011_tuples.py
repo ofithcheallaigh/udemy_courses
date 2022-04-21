fe283a8bb9724412694d20818acc04146e9bef6b
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-04-21 20:46:44 +0100

    adding updated tuples file

--- The Python Bible/Code/011_tuples.py
+++ The Python Bible/Code/011_tuples.py
@@ -4,3 +4,17 @@
 
 our_tuple = 1,2,3,'A','B','C'
 print(our_tuple)
+
+# More typically, tuples are created with brackets:
+our_tuple = (1,2,3,'A','B','C')
+print(type(our_tuple))
+
+# We can slice tuples, in the same way as lists:
+sliced_tuple = our_tuple[0:3]
+print(sliced_tuple)
+
+# We can use the tuple function to turn other data into a tuple
+a_list = [1,2,3]
+print(type(a_list))
+a_list = tuple(a_list)
+print(type(a_list))
