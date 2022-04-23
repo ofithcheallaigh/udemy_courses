4eba42a2ded6e27065c9edc140f874f172b6509c
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-04-23 21:09:24 +0100

    updating dict to hold more info

---
+++ The Python Bible/Code/013_dictionaries_2.py
@@ -0,0 +1,12 @@
+# set up dictionary
+students = {"Alice": ["ID0001", 26, "A"], 
+            "Bob": ["ID0002", 27, "B"],  
+            "Claire": ["ID0003", 18, "C"], 
+            "Dan": ["ID0004", 21, "D"], 
+            "Emma": ["ID0005", 22, "E"]
+            }
+
+print(students["Claire"])
+
+
+
